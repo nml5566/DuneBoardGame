e30f078decda8dd81a4781d70cc7952d3d97f457
Nathan Liles <nml5566@gmail.com>
2014-01-17 20:39:10 -0500

    Faction select view now launches map view and passes faction list to it.

--- public/js/game.js
+++ public/js/game.js
@@ -1,75 +1,233 @@
 window.onload = function() {
-  var gameView = new GameView();
-  gameView.init();
+ new GameView();
 }
 
 function GameView() {
-  this.init = init;
+
+  var startView = new StartView();
+  var factionSelectView = new FactionSelectView();
+
+  init();
 
   function init() {
-    initPlayButton();
-    initFactionSelector();
+    addPlayClickEvent();
   }
 
-  function initPlayButton() {
+  function addPlayClickEvent() {
     var playButton = document.getElementById("playbutton");
-
-    playButton.onmouseover = function() {
-      this.style.opacity = 0.6;
-    }
-
-    playButton.onmouseout = function() {
-      this.style.opacity = 1.0;
-    }
-    playButton.onclick = playGame;
-      
-    function playGame() {
-
-      var startScreen = document.getElementById("gamestartscreen");
-      startScreen.style.display = "none";
-
-      var factionSelectScreen = document.getElementById("factionselectscreen");
-      factionSelectScreen.style.display = "block";
-    }
+    playButton.onclick = showFactionSelectView;
   }
 
-  function initFactionSelector() {
-    var factionSelectIcon = "faction-select.png";
-    var factionIcons = new Array(
-	"atreides-emblem125x125.png",
-	//"atreides-shield200x224.png",
-    	"fremen-emblem125x125.png",
-    	"guild-emblem125x125.png",
-    	"emperor-emblem125x125.png",
-    	"harkonnen-emblem125x125.png",
-    	"benegesserit-emblem125x125.png",
-    	factionSelectIcon
-    );
+  function showFactionSelectView() {
+    startView.hide();
+    factionSelectView.show();
+  }
 
-    var factionSelect = document.getElementById("factionselectcontainer");
+}
 
+function StartView() {
+
+  var view = document.getElementById("gamestartscreen");
+
+  this.show = hide;
+  this.hide = hide;
+
+  function show() {
+    view.style.display = "block";
+  }
+
+  function hide() {
+    view.style.display = "none";
+  }
+}
+
+function FactionSelectView() {
+
+  var container = document.getElementById("factionselectcontainer");
+  var view = document.getElementById("factionselectscreen");
+
+  var factionSelectIcon = "faction-select.png";
+  var factionSelect = "faction-select";
+
+  var factions = new Array(
+      "Atreides",
+      "Fremen",
+      "Guild",
+      "Emperor",
+      "Harkonnen",
+      "BeneGesserit",
+      factionSelect
+  );
+
+  var factionIcons = new Array(
+      "atreides-emblem125x125.png",
+      //"atreides-shield200x224.png",
+      "fremen-emblem125x125.png",
+      "guild-emblem125x125.png",
+      "emperor-emblem125x125.png",
+      "harkonnen-emblem125x125.png",
+      "benegesserit-emblem125x125.png",
+      factionSelectIcon
+  );
+  var factionImages = new Array();
+
+  var startButton;
+
+  var me = this;
+  this.show = show;
+  this.hide = hide;
+
+  init();
+
+  function show() {
+    view.style.display = "block";
+  }
+
+  function hide() {
+    view.style.display = "none";
+  }
+
+  function init() {
+    makeFactionImageElements();
+    makeBackButtonElement();
+    makeStartButtonElement();
+  }
+
+  function makeFactionImageElements() {
     for (var i = 0; i < 6; i++) {
       var image = new Image();
       image.src = "/img/icons/" + factionSelectIcon;
-      image.onclick = selectFactions;
+      //image.onclick = factionSelector;
+      image.onclick = factionSelector2;
 
-      factionSelect.appendChild(image);
+      container.appendChild(image);
+      factionImages.push(image);
+    }
+  }
+
+  function makeBackButtonElement() {
+    var backButton = new Image();
+    backButton.src = "/img/icons/back.png";
+    container.appendChild(backButton);
+    backButton.onclick = returnToStartScreen;
+
+    function returnToStartScreen() {
+      var startScreen = document.getElementById("gamestartscreen");
+      startScreen.style.display = "block";
+
+      var factionSelectScreen = document.getElementById("factionselectscreen");
+      factionSelectScreen.style.display = "none";
+    }
+  }
+
+  function makeStartButtonElement() {
+    startButton = new Image();
+    startButton.src = "/img/icons/start.png";
+    startButton.style.opacity = 0.6;
+    startButton.style.cursor = "default";
+    container.appendChild(startButton);
+  }
+
+
+  function factionSelector() {
+    var icon = factionIcons.shift();
+
+    if (this.selectedFaction) 
+      factionIcons.push(this.selectedFaction);
+
+    if (icon === factionSelectIcon) {
+      delete this.selectedFaction; 
+      factionIcons.push(factionSelectIcon);
+    } else {
+      this.selectedFaction = icon;
     }
 
-    function selectFactions() {
-      var icon = factionIcons.shift();
+    this.src = "/img/icons/" + icon;
 
-      if (this.factionInUse) 
-	factionIcons.push(this.factionInUse);
+    checkIfGameReadyToStart();
+  }
 
-      if (icon === factionSelectIcon) {
-	delete this.factionInUse; 
-	factionIcons.push(factionSelectIcon);
-      } else {
-	this.factionInUse = icon;
-      }
+  function factionSelector2() {
+    var faction = factions.shift();
 
-      this.src = "/img/icons/" + icon;
+    if (this.selectedFaction) 
+      factions.push(this.selectedFaction);
+
+    if (faction === factionSelect) {
+      delete this.selectedFaction; 
+      factions.push(factionSelect);
+    } else {
+      this.selectedFaction = faction;
     }
+
+    this.src = "/img/icons/" + faction + "125x125.png";
+
+    checkIfGameReadyToStart();
+  }
+
+  function checkIfGameReadyToStart() {
+    if (isGameReadyToStart()) {
+      enableStartButton();
+    } else {
+      disableStartButton();
+    }
+
+  }
+
+  function isGameReadyToStart() {
+    if (factions.length <= 5) {
+      return 1
+    } else if (factions.length > 5) {
+      return 0
+    }
+  }
+
+  function enableStartButton() {
+    startButton.style.opacity = 1.0;
+    startButton.style.cursor = "pointer";
+    startButton.onclick = startGame;
+  }
+
+  
+  function disableStartButton() {
+    startButton.style.opacity = 0.6;
+    startButton.style.cursor = "default";
+    startButton.onclick = null; 
+  }
+
+  function startGame() {
+    me.hide();
+    var factions = getSelectedFactions();
+    var mapView = new MapView(factions);
+    mapView.show();
+  }
+
+  function getSelectedFactions() {
+    var factions = new Array();
+    for (var i = 0; i < factionImages.length; i++) {
+      var image = factionImages[i];
+      if (! image.selectedFaction) 
+      	continue;
+
+      factions.push(image.selectedFaction);
+    }
+    return factions;
+  }
+
+}
+
+function MapView(factions) {
+
+  var view = document.getElementById("mapscreen");
+
+  this.show = show;
+
+  init();
+
+  function show() {
+    view.style.display = "block";
+  }
+
+  function init() {
   }
 }
