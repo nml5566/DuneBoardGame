3508339c9c2b99a95e0423227cf921d3f8a8a5d6
Nathan Liles <nml5566@gmail.com>
2014-01-24 00:35:52 -0500

    Clicking Arakeen/Carthag shows enlarged territories

--- client/main.js
+++ client/main.js
@@ -12,52 +12,33 @@ window.onload = function() {
   testTerritoryClick();
 }
 
+var Loader = require("Dune/Loader");
+var canvasContainer = require("Dune/CanvasContainer");
+var mapView = require("Dune/View/Map");
+
 function testTerritoryClick()
 {
-  //var map = document.getElementsByName("map")[0];;
-  //console.log(map);
 
-/*  var canvasContainer = require("Dune/CanvasContainer");*/
-  //var canvas = canvasContainer.getTopLayer();
+  var loader = new Loader();
 
+  var territoryImages = {
+    "carthag": "/img/territories/carthag.png",
+    "arrakeen": "/img/territories/arrakeen.png"
+  };
 
-  //var ctx = canvas.getContext("2d");
+  for (territoryName in territoryImages) {
+    var territoryUrl = territoryImages[territoryName];
+    territoryImages[territoryName] = loader.loadImage(territoryUrl);
+  }
 
-  //var territories = [ 
-    //[
-      //"Cielago North",
-      //[411,473,395,485,373,485,361,467,301,571,303,576,303,579,320,594,361,598,367,593,383,594,418,616,429,608,441,588,450,579,458,554]
-    //],
-    //[
-      //"Polar Sink",
-      //[374,484,368,476,361,466,350,466,341,462,332,454,327,454,321,446,324,425,333,408,348,397,366,395,379,391,384,387,390,387,397,394,409,394,413,392,425,392,436,405,434,425,424,439,418,453,411,461,410,473,395,484]
-    //]
-  //];
-
- 
-  //canvas.addEventListener('click', function(e) {
-    //var coord = getMousePosition(this,e);
-
-    //for (var i = 0; i < territories.length; i++) 
-    //{
-      //var territory = territories[i];
-      //var name = territory[0];
-      //var coords = territory[1];
-
-      //drawTerritoryOutline(coords, ctx);
-
-      //if (ctx.isPointInPath(coord.x, coord.y)) 
-	//console.log('clicked ' + name);
-    //}
-
-  /*});*/
-
-  
   var areaTags = document.getElementsByTagName("area");
   for (var i = 0; i < areaTags.length; i++) {
     var areaTag = areaTags[i];
     areaTag.addEventListener('click', function(e) {
       console.log(this.target);
+      console.log(territoryImages[this.target]);
+      if (! territoryImages[this.target]) return;
+      enlargeTerritory(territoryImages[this.target]);
     });
   }
   console.log(areaTags);
@@ -65,7 +46,21 @@ function testTerritoryClick()
   var territoryScreen = document.getElementById("territoryscreen");
   territoryScreen.style.display = "block";
   territoryScreen.style.zIndex = 200;
-  /*console.log(test);*/
+}
+
+function enlargeTerritory(territoryImage) {
+  console.log('enlarging territory');
+  var canvas = canvasContainer.layer('notification');
+  var context = canvas.getContext("2d");
+
+  var circle = mapView.circle;
+
+  var imageScaleWidth = circle.radius * 2;
+  var imageScaleHeight = circle.radius * 2;
+
+  context.drawImage(territoryImage, 
+      circle.centerX - territoryImage.width/2, 
+      circle.centerY - territoryImage.height/2);
 }
 
 function drawTerritoryOutline(coords, ctx) 
